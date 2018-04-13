7451434c0a1a268bddfa80cfbad96011aa179baa
dkotsuka <dkotsuka@gmail.com>
2018-04-13 08:22:42 -0300

    update do projeto 011

--- Projeto011/script.js
+++ Projeto011/script.js
@@ -1,6 +1,14 @@
-let botao = document.querySelector('button');
+let height = document.querySelector('#altura');
+let weight = document.querySelector('#peso');
+let send = document.querySelector('#enviar');
+let back = document.querySelector('#voltar');
+let calc_content = document.querySelector('calcular');
+let result_canvas = document.querySelector('#resultado');
+let result_index_display = document.querySelector('resultado_numero');
+let result_text_display = document.querySelector('resultado_texto');
 
-botao.addEventListener('click', function(){
-	let cor = document.querySelector('#cor').value;
-	document.querySelector('body').style.backgroundColor = cor;
+send.addEventListener('click', function(height, weight){
+	let imc = weight / (height*height);
+	let nodes = calc_content.childNodes;
+	console.log(nodes);
 });
